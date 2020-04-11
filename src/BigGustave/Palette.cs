b2ea071432399bf4f58aa91976a1623e6a60db47
Dominic Grenier <themoustafa@hotmail.com>
2020-04-11 15:56:32 -0400

    Get the right palette index, trns should work?

--- src/BigGustave/Palette.cs
+++ src/BigGustave/Palette.cs
@@ -1,22 +1,39 @@
 ﻿namespace BigGustave
-{    using System;
+{   
+    using System;
     internal class Palette
     {
-        public byte[] Data { get; }
+        public byte[] Data { get; set;}
+        public byte[] Transparency { get; set;}
 
-        public Palette(byte[] data)
+        public Palette(byte[] data, byte[] transparency)
         {
             Data = data;
+            Transparency = transparency;
+
         }
 
-        public Pixel GetPixel(int index, byte[] imgdat, int bytesPerPixel)
+        public Pixel GetPixel(int index, byte[] imgdat)
         {
-            var start = imgdat[index % imgdat.Length] % (Data.Length / 3);
+            var start = (imgdat[index % imgdat.Length] % (Data.Length / 3)) * 3;
 
+            byte[] pix = {Data[start], Data[start + 1], Data[start + 2]};
 
-            return new Pixel(Data[start * 3],
-                Data[start * 3 + 1],
-                Data[start * 3 + 2], 255, false);
+            byte trns = 255;
+
+            // Console.WriteLine(Transparency[0] + ", " + Transparency[1] + ", " + Transparency[2]);
+
+            if(Transparency != null){
+                if(pix[0] == Transparency[0] &&
+                   pix[1] == Transparency[1] &&
+                   pix[2] == Transparency[2]){
+                    trns = 0;
+                }
+            }
+
+            return new Pixel(Data[start],
+                Data[start + 1],
+                Data[start + 2], trns, false);
         }
     }
 }
